ee70e04254a7167d183a4a65bff058caeda53c5a
Aadit Yadav <aadityadav121@gmail.com>
2026-02-03 17:17:56 +0530

    Day-9 created all four API's in the backend

---
+++ Day-9/Backend/src/app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const notesModel = require("./models/notes.models");
+
+const app = express();
+app.use(express.json());
+
+/*
+POST /api/notes
+*/
+app.post("/api/notes", async (req, res) => {
+  const { title, description } = req.body;
+  const notes = await notesModel.create({
+    title,
+    description,
+  });
+  res.status(201).json({
+    message: "Note created successfully",
+    notes,
+  });
+});
+
+/*
+- GET /api/notes
+*/
+app.get("/api/notes", async (req, res) => {
+  const notes = await notesModel.find();
+
+  res.status(200).json({
+    message: "Notes featched successfully",
+    notes,
+  });
+});
+
+/*
+- DELETE /api/notes/:id
+*/
+app.delete("/api/notes/:id", async (req, res) => {
+  const id = req.params.id;
+
+  await notesModel.findByIdAndDelete(id);
+
+  res.status(200).json({
+    message: "Note deleted successfully",
+  });
+});
+
+/*
+- PATCH /api/notes/:id
+*/
+app.patch("/api/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  const { description } = req.body;
+
+  await notesModel.findByIdAndUpdate(id, { description });
+
+  res.status(200).json({
+    message: "Note updated successfully",
+  });
+});
+
+module.exports = app;
