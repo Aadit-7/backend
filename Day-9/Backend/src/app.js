f1caf58c5027a48c25301a82b724bbc3b6f51d81
Aadit Yadav <aadityadav121@gmail.com>
2026-02-04 19:25:28 +0530

    Day-9 learned how to connect frontend with the backend using axios and why to use cors

--- Day-9/Backend/src/app.js
+++ Day-9/Backend/src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const notesModel = require("./models/notes.models");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 /*
