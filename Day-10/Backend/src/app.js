1db7ac5f6b5bcbbc244f71bb4cf963b2197ffe03
Aadit Yadav <aadityadav121@gmail.com>
2026-02-05 17:33:42 +0530

    Day-10 learned how to mearge frontend and backend with dist folder

--- Day-10/Backend/src/app.js
+++ Day-10/Backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const noteModel = require("./models/notes.model");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("./public"))
 
 /*
 POST /api/notes
@@ -60,5 +62,9 @@ app.patch("/api/notes/:id", async (req, res) => {
   res.status(200).json({
     msg: "Note updated successfully",
   });
+});
+
+app.use("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 module.exports = app;
