fbe44ae0df4c434118ee4c39a1da4e459448fc92
Aadit Yadav <aadityadav121@gmail.com>
2026-02-17 18:36:24 +0530

    Day-4 Fetch post and get post details APIs done

--- Day-14/src/controllers/post.constroller.js
+++ Day-14/src/controllers/post.constroller.js
@@ -48,6 +48,81 @@ async function createPostController(req, res) {
   });
 }
 
+async function fetechPostController(req, res) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({
+      msg: "Token is not provided. Login again",
+    });
+  }
+
+  let decoded;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECREAT);
+  } catch (error) {
+    return res.status(401).json({
+      msg: "Token is not provided",
+    });
+  }
+  const userId = decoded.id;
+  const post = await postModel.find({
+    user: userId,
+  });
+
+  res.status(200).json({
+    msg: "Posts featched successfully",
+    post,
+  });
+}
+
+async function getPostDetailsController(req, res) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({
+      msg: "Token is not provided. Login again",
+    });
+  }
+
+  let decoded;
+
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECREAT);
+  } catch (error) {
+    res.status(401).json({
+      msg: "No token provided",
+    });
+  }
+
+  const userId = decoded.id;
+  const postId = req.params.postId;
+
+  const post = await postModel.findById(postId);
+
+  if (!post) {
+    return res.status(404).json({
+      msg: "Page not Found",
+    });
+  }
+
+  // console.log(post.user === userId);
+
+  const isUserValidate = post.user.toString() === userId;
+
+  if (!isUserValidate) {
+    return res.status(403).json({
+      msg: "Forbidden Contend (You are not allowed to see this post)",
+    });
+  }
+
+  res.status(200).json({
+    msg: "Post fetched Successfully",
+    post,
+  });
+}
 module.exports = {
   createPostController,
+  fetechPostController,
+  getPostDetailsController,
 };
--- Day-14/src/routes/post.route.js
+++ Day-14/src/routes/post.route.js
@@ -5,10 +5,23 @@ const postController = require("../controllers/post.constroller");
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
 
+/*
+POST /api/  - create post
+*/
 postRouter.post(
   "/",
   upload.single("image"),
   postController.createPostController,
 );
+
+/*
+GET /api/  - create post
+*/
+postRouter.get("/", postController.fetechPostController);
+
+/*
+GET /api/details:postId  - create post
+*/
+postRouter.get("/details/:postId", postController.getPostDetailsController)
 
 module.exports = postRouter;
