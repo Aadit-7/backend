20776d90b0c15157e5e5978487c8485d2f8a6fa7
Aadit Yadav <aadityadav121@gmail.com>
2026-06-17 14:31:59 +0530

    Practise done

--- Day-11.1/Backend/src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { registerValidator } from "../validators/auth.validator.js";
-
-const authRouter = Router();
-
-authRouter.post("/register", registerValidator);
-
-export default authRouter;
